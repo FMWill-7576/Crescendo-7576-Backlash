eb576aa911f89ef8ca6b19ce7d2cb445f0919824
redbread <yigit.olc@hotmail.com>
2024-03-01 19:00:12 +0300

    configuring shooter

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -12,6 +12,7 @@ import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Timer;
@@ -27,20 +28,27 @@ public class Shooter extends SubsystemBase {
   private CANSparkMax shootMotorUp;
   private CANSparkMax shootMotorDown;
   private RelativeEncoder integratedUpEncoder;
+  private RelativeEncoder integratedDownEncoder;
   private Encoder shooterBore;
-  private RelativeEncoder shooterRelative;
+  private RelativeEncoder shooterRelativeBore;
   private final SparkPIDController shootController ;
+   private final SparkPIDController shootControllerDown ;
   private PIDController shooterPID;
+   private SimpleMotorFeedforward shooterFF;
   private double setpoint;
+  private double downSetpoint;
   /** Creates a new shootator. */
   public Shooter() {
     shootMotorUp= new CANSparkMax(12, MotorType.kBrushless);
     shootMotorDown= new CANSparkMax(13, MotorType.kBrushless);
     shootController = shootMotorUp.getPIDController();
+    shootControllerDown = shootMotorDown.getPIDController();
     shooterPID = new PIDController(0, 0, 0);
+    shooterFF = new SimpleMotorFeedforward(0, 0);
     integratedUpEncoder = shootMotorUp.getEncoder();
+    integratedDownEncoder = shootMotorDown.getEncoder();
     //shooterBore = new Encoder(0, 0, false, EncodingType.k1X);
-    shooterRelative = shootMotorUp.getAlternateEncoder(2048);
+    shooterRelativeBore = shootMotorUp.getAlternateEncoder(2048);
     shooterConfig();
   
   }
@@ -48,27 +56,45 @@ public class Shooter extends SubsystemBase {
   public void shooterConfig() { 
     shootMotorUp.restoreFactoryDefaults();
     shootMotorDown.restoreFactoryDefaults();
-    shootController.setFeedbackDevice(shooterRelative);
+   // shootController.setFeedbackDevice(shooterRelative);
+    shootController.setFeedbackDevice(integratedUpEncoder);
+    shootControllerDown.setFeedbackDevice(integratedDownEncoder);
     CANSparkMaxUtil.setCANSparkMaxBusUsage(shootMotorUp, Usage.kVelocityOnly,true);
     CANSparkMaxUtil.setCANSparkMaxBusUsage(shootMotorDown, Usage.kVelocityOnly,true);
-    shootMotorUp.setSmartCurrentLimit(45);
-    shootMotorDown.setSmartCurrentLimit(45);
+    shootMotorUp.setSmartCurrentLimit(40);
+    shootMotorDown.setSmartCurrentLimit(40);
     shootMotorUp.setInverted(false);
+    shootMotorDown.setInverted(false);
     shootMotorUp.setIdleMode(IdleMode.kCoast);
-    shootMotorUp.setIdleMode(IdleMode.kCoast);
+    shootMotorDown.setIdleMode(IdleMode.kCoast);
+    shooterRelativeBore.setVelocityConversionFactor(1.0);
     integratedUpEncoder.setPositionConversionFactor(1.0); 
     integratedUpEncoder.setPosition(0.0);
-    integratedUpEncoder.setVelocityConversionFactor(1.0);  
-    shootController.setP(0.0);
+    integratedDownEncoder.setPosition(0.0);
+    integratedUpEncoder.setVelocityConversionFactor(1.6);  
+    integratedDownEncoder.setVelocityConversionFactor(1.6); 
+    shootController.setP(0.0001);
     shootController.setI(0.0);
     shootController.setD(0.0);
-    shootController.setFF(0.0);
+    shootController.setFF(0.00012);
     shootController.setIMaxAccum(0.0,0);
     shootController.setIZone(0.0);
+    shootControllerDown.setP(0.0001);
+    shootControllerDown.setI(0.0);
+    shootControllerDown.setD(0.0);
+    shootControllerDown.setFF(0.00012);
+    shootControllerDown.setIMaxAccum(0.0,0);
+    shootControllerDown.setIZone(0.0);
     shootMotorUp.enableVoltageCompensation(12.0);
+    shootMotorDown.enableVoltageCompensation(12.0);
+    shootMotorUp.setClosedLoopRampRate(0.05);
+    shootMotorDown.setClosedLoopRampRate(0.05);
+    shootMotorUp.setOpenLoopRampRate(0.05);
+    shootMotorDown.setOpenLoopRampRate(0.05);
     shootController.setOutputRange(-1.0, 1.0);
+    shootControllerDown.setOutputRange(-1.0, 1.0);
     //integratedArmEncoder.setReverseDirection(false); // bu
-    shootMotorDown.follow(shootMotorUp);
+    //shootMotorDown.follow(shootMotorUp);
     Timer.delay(0.2);
     shootMotorUp.burnFlash();
     Timer.delay(0.2);
@@ -78,23 +104,37 @@ public class Shooter extends SubsystemBase {
 
 public void shootDriveManual(double percentage){
 shootMotorUp.set(percentage);
+shootMotorDown.set(percentage);
 }
 
 public void shooterSet(double RPM) {
+  downSetpoint=RPM+400;
+  setpoint=RPM;
 
         shootController.setReference(
-         RPM,
+         setpoint,
+         CANSparkMax.ControlType.kVelocity,
+         0
+        );
+
+
+        shootControllerDown.setReference(
+         downSetpoint,
          CANSparkMax.ControlType.kVelocity,
          0
         );
          
-       setpoint=RPM;
       }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    SmartDashboard.putNumber("shooter rpm", shooterRelative.getVelocity());
+    SmartDashboard.putNumber("shooter measured rpm up BORE", shooterRelativeBore.getVelocity());
+    SmartDashboard.putNumber("shooter measured rpm up", integratedUpEncoder.getVelocity());
+    SmartDashboard.putNumber("shooter measured rpm down", integratedDownEncoder.getVelocity());
+    SmartDashboard.putNumber("shooter setpoint rpm", setpoint);
     SmartDashboard.putNumber("shooter applied duty", shootMotorUp.getAppliedOutput());
+    SmartDashboard.putNumber("shooter bus voltage", shootMotorUp.getBusVoltage());
+    SmartDashboard.putNumber("shooter applied amp", shootMotorUp.getOutputCurrent());
   }
 }
