40a7ba040ece6f593aa3cbcf956def3ab6ffbf47
redbread <yigit.olc@hotmail.com>
2024-03-01 18:59:00 +0300

    cleanup + telemetry arm

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -5,6 +5,8 @@ import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.PowerDistribution;
+import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -31,6 +33,7 @@ public class Arm extends SubsystemBase {
   private CANSparkMax armMotor;
   //private CANSparkMax armMotor2;
   private final SparkPIDController armController;
+  private final PowerDistribution examplePD;
   //private final SparkPIDController armController2;
   private RelativeEncoder integratedArmEncoder;
  // private RelativeEncoder integratedArmEncoder2;
@@ -48,6 +51,7 @@ public class Arm extends SubsystemBase {
 
   public Arm() {
       armMotor  = new CANSparkMax(11, MotorType.kBrushless);
+       examplePD = new PowerDistribution(0, ModuleType.kCTRE);
       //armMotor2  = new CANSparkMax(65, MotorType.kBrushless); 
       armController = armMotor.getPIDController();
      // armController2 = armMotor2.getPIDController();
@@ -64,21 +68,12 @@ public class Arm extends SubsystemBase {
 
       public void armMotorConfig() { 
         armMotor.restoreFactoryDefaults();
-      //  armMotor2.restoreFactoryDefaults();
         CANSparkMaxUtil.setCANSparkMaxBusUsage(armMotor, Usage.kPositionOnly,true);
-       // CANSparkMaxUtil.setCANSparkMaxBusUsage(armMotor2, Usage.kPositionOnly,true);
         armMotor.setSmartCurrentLimit(40);
-       // armMotor2.setSmartCurrentLimit(40);
         armMotor.setInverted(true);
         armMotor.setIdleMode(IdleMode.kBrake);
-      //  armMotor2.setInverted(true);
-      //  armMotor2.setIdleMode(IdleMode.kBrake); 
         integratedArmEncoder.setPositionConversionFactor((1/64) * (22/68)); 
         integratedArmEncoder.setPosition(-46.41);
-       // integratedArmEncoder2.setPositionConversionFactor(7.742); 
-       // integratedArmEncoder.setVelocityConversionFactor(7.742); 
-       // integratedArmEncoder2.setPosition(-64.5); 
-       // integratedArmEncoder2.setVelocityConversionFactor(7.742);
         throughbEncoder.setPositionConversionFactor(180/(68/22));
         throughbEncoder.setPosition(-46.41);   // -47.41
         throughbEncoder.setInverted(true);
@@ -88,13 +83,6 @@ public class Arm extends SubsystemBase {
         armController.setIMaxAccum(Constants.ArmConstants.armKIMax,0);
         armController.setIZone(Constants.ArmConstants.armKIZone);
         armController.setFeedbackDevice(throughbEncoder);
-      //  armController2.setP(Constants.ArmConstants.armKP);
-      //  armController2.setI(Constants.ArmConstants.armKI);
-      //  armController2.setD(Constants.ArmConstants.armKD);
-      //  armController2.setIMaxAccum(Constants.ArmConstants.armKIMax,0);
-      //  armController2.setIZone(Constants.ArmConstants.armKIZone);
-     //   armController2.setOutputRange(-0.48, 0.48);
-      //  armController2.setFeedbackDevice(integratedArmEncoder2);
         armMotor.enableVoltageCompensation(12.0);
         armMotor.enableSoftLimit(SoftLimitDirection.kForward, true);
         armMotor.setSoftLimit(SoftLimitDirection.kForward, 65);
@@ -102,10 +90,7 @@ public class Arm extends SubsystemBase {
         armMotor.setSoftLimit(SoftLimitDirection.kReverse, -47);
 
         armMotor.setClosedLoopRampRate(0.15);
-        armMotor.setOpenLoopRampRate(0.1);
-  
-      //  armMotor2.enableVoltageCompensation(12.0);
-      //  armMotor2.follow(armMotor);
+        armMotor.setOpenLoopRampRate(0.12);
      
         armController.setOutputRange(-1.0, 1.0);
         //integratedArmEncoder.setReverseDirection(false); // bu
@@ -142,9 +127,9 @@ public class Arm extends SubsystemBase {
        (36.2));
       }
 
-     public void armGrip(){
+     public void armTest2(){
       armSet(Rotation2d.fromDegrees
-      (239.5));
+      (20));
     }
     public void armPickUp(){
       armSet(Rotation2d.fromDegrees
@@ -159,6 +144,7 @@ public class Arm extends SubsystemBase {
 
      public void armDrive(double armPercentage){
       armMotor.set(armPercentage);
+    
       //armMotor2.set(armPercentage);
     }    
     
@@ -190,12 +176,14 @@ public class Arm extends SubsystemBase {
     public void periodic() {
   
       // This method will be called once per scheduler run
-      SmartDashboard.putNumber("arm encoder" , integratedArmEncoder.getPosition());
-      SmartDashboard.putNumber("bore encoder" , throughbEncoder.getPosition());
+      SmartDashboard.putNumber("integrated arm encoder" , integratedArmEncoder.getPosition());
+      SmartDashboard.putNumber("arm encoder" , throughbEncoder.getPosition());
       SmartDashboard.putNumber("setpoint",setpoint);
       //SmartDashboard.putNumber("arm speed",integratedArmEncoder.getVelocity());
-      SmartDashboard.putNumber("arm output", armMotor.getAppliedOutput());
+      SmartDashboard.putNumber("arm output duty", armMotor.getAppliedOutput());
        SmartDashboard.putNumber("arm output amps", armMotor.getOutputCurrent());
-
+       SmartDashboard.putNumber("arm output voltage", armMotor.getBusVoltage());
+     //  SmartDashboard.putNumber("arm nominal voltage", armMotor.getVoltageCompensationNominalVoltage());
+       SmartDashboard.putNumber("pdp voltage", examplePD.getVoltage());
     }
 }
