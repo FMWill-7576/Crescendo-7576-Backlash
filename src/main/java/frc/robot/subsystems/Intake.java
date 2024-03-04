eff8da07562e986a3e1a09a555b93c055ed8bc0b
redbread <yigit.olc@hotmail.com>
2024-03-04 20:25:30 +0300

    intake logging and ramp rate

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -12,6 +12,7 @@ import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.lib.util.CANSparkMaxUtil;
 import frc.lib.util.CANSparkMaxUtil.Usage;
@@ -30,7 +31,7 @@ intakeConfig();
   }
 
   public void intake(){
-  intakeMotor.set(0.75);
+  intakeMotor.set(1.0);
   }
 
   public void   outake(){ 
@@ -38,13 +39,19 @@ intakeMotor.set(-0.5);
   }
 
 
+ public void manualIntake(double speed){
+  intakeMotor.set(speed);
+ }
+
 
   public void intakeConfig(){
   intakeMotor.restoreFactoryDefaults();
   intakeMotor.enableVoltageCompensation(12);
-  intakeMotor.setSmartCurrentLimit(30);
+  intakeMotor.setSmartCurrentLimit(40);
   intakeMotor.setInverted(false);
   intakeMotor.setIdleMode(IdleMode.kCoast);
+  intakeMotor.setClosedLoopRampRate(0.15);
+  intakeMotor.setOpenLoopRampRate(0.15);
   CANSparkMaxUtil.setCANSparkMaxBusUsage(intakeMotor, Usage.kMinimal, false);
   Timer.delay(0.2);
   intakeMotor.burnFlash();
@@ -54,6 +61,9 @@ intakeMotor.set(-0.5);
 
   @Override
   public void periodic() {
+    SmartDashboard.putNumber("intake output duty", intakeMotor.getAppliedOutput());
+    SmartDashboard.putNumber("intake output amps", intakeMotor.getOutputCurrent());
+    SmartDashboard.putNumber("intake bus voltage", intakeMotor.getBusVoltage());
     // This method will be called once per scheduler run
   }
 }
